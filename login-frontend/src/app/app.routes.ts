49be01569ae341f0e19f536efe35dc8ecf2cc95e
Murtaza <mpirzz.7@gmail.com>
2026-02-06 12:38:27 -0500

    Original Dashboard design

--- login-frontend/src/app/app.routes.ts
+++ login-frontend/src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { Login } from './login/login'; // Adjusted to point to the TypeScript co
 import { Home } from './home/home';
 import { authGuard } from './auth.guard';
 import { guestGuard } from './guest.guard';
+import { ProductsComponent } from './home/products/products.component'; // Added import for Products component
 
 export const routes: Routes = [
     { path: 'login', component: Login, canActivate: [guestGuard]},
-    { path: 'home', component: Home, canActivate: [authGuard] },
+    { path: 'home', component: Home, canActivate: [authGuard], children: [
+        {path: 'products', component: ProductsComponent},
+    ]},
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
--- login-frontend/src/app/home/home.ts
+++ login-frontend/src/app/home/home.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, signal } from '@angular/core';
 import { Auth } from '../auth/auth';
 import { Router } from '@angular/router';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LeftSidebarComponent, MainComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   constructor(private auth: Auth, private router: Router) {}
+
+  isLeftSidebarCollapsed = signal<boolean>(false);
+  screenWidth = signal<number>(window.innerWidth);
+
+  @HostListener('window:resize')
+  onResize() {
+    this.screenWidth.set(window.innerWidth);
+    if (this.screenWidth() < 768) {
+      this.isLeftSidebarCollapsed.set(true);
+    }
+  }
+
+  ngOnInit(): void {
+    this.isLeftSidebarCollapsed.set(this.screenWidth() < 768);
+  }
+
+  changeIsLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean): void {
+    this.isLeftSidebarCollapsed.set(isLeftSidebarCollapsed);
+  }
 
   logout() {
     this.auth.logout();
